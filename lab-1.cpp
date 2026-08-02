2521e0c2a79215a22c534f2007205b68d55e47ae
Whatyadoinn <minehome205@gmail.com>
2026-08-02 10:56:46 +0530

    Pushed

--- lab-1.cpp
+++ lab-1.cpp
@@ -1,3 +1,4 @@
+// OOPS
 // Part 1
 
 #include <iostream>
@@ -294,3 +295,94 @@ using namespace std;
 //     return 0;
 // }
 
+// Part 7
+
+// class Complex{
+//     private:
+//         float real;
+//         float imaginary;
+
+//     public:
+//         void set(){
+//               cout << "Enter real part: ";
+//         cin >> real;
+//         cout << "Enter imaginary part: ";
+//         cin >> imaginary;
+//         }
+
+//         void display(){
+//             cout<<real;
+//             if (imaginary>=0)
+//             {
+//                 cout<< " + " << imaginary << "i" << endl;
+//             }
+//             else
+//             {
+//                 cout<< "-" << -imaginary << "i" << endl;
+//             }
+            
+//         }
+
+//         Complex sum(Complex c) {
+       
+//         Complex result;
+//         result.real = real + c.real;
+//         result.imaginary = imaginary + c.imaginary;
+//         return result;
+//                         }
+// };
+
+// int main(){
+//     Complex c1,c2,c3;
+
+
+//     cout << "Enter first complex number:" << endl;
+//     c1.set();
+
+//     cout << "Enter second complex number:" << endl;
+//     c2.set();
+
+//     c3=c1.sum(c2);
+
+//     cout<<"\n First Complex Number:";
+//     c1.display();
+
+//     cout<<"\n Second Complex Number:";
+//     c2.display();
+
+//      cout<<"\n Sum of Complex Number:";
+//      c3.display();
+
+//      return 0;
+// }
+
+// Part 8
+
+// #include <iostream>
+// using namespace std;
+
+// namespace India {
+//     int price = 50;
+
+//     void show() {
+//         cout << "Price in India: " << price << " rupees" << endl;
+//     }
+// }
+
+// namespace USA {
+//     int price = 5;
+
+//     void show() {
+//         cout << "Price in USA: " << price << " dollars" << endl;
+//     }
+// }
+
+// int main() {
+//     India::show();
+//     USA::show();
+
+//     cout << "India price: " << India::price << endl;
+//     cout << "USA price: " << USA::price << endl;
+
+//     return 0;
+// }
